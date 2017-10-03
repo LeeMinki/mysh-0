9e0927c8a3b5ccc014f1869064147fc20f98cebe
Lee Minki <curyowo@gmail.com>
2017-10-03 22:30:08 +0900

    Maybe, this is final version!

--- src/commands.c
+++ src/commands.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include "commands.h"
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int do_cd(int argc, char** argv) {
 	if (!validate_cd_argv(argc, argv))
@@ -21,9 +23,18 @@ int do_pwd(int argc, char** argv) {
 }
 
 int validate_cd_argv(int argc, char** argv) {
-	if(argc!=2||strcmp(argv[0],"cd")!=0)
-		return 0;
-	return 1;
+	if(argc==2&&strcmp(argv[0],"cd")==0)
+	{
+		int result = access(argv[1], F_OK);
+		if(result == 0)
+		{
+			struct stat buf;
+			stat(argv[1], &buf);
+			if(S_ISDIR(buf.st_mode))
+				return 1;
+		}
+	}	
+	return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
